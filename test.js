7bf3f432e1b32282abd866821a871cf9b452f063
DENR1908 <navarrodixon.p21@gmail.com>
2024-05-21 00:38:35 -0300

    Avance

--- test.js
+++ test.js
@@ -1,22 +1,42 @@
-let opciones = prompt ("Bienvenido.\nSeleccione la Opción deseada.\n1- Calcular Préstamo(1 Pago)\n2- Calcular Prestamos 6 Cuotas Fijas  "); 
+let opciones = prompt("Bienvenido.\nSeleccione la Opción deseada.\n1- Calcular Préstamo(1 Pago)\n2- Calcular Prestamos 6 Cuotas Fijas\n3-Salir."); 
 let importe = 0;
+let cuotas = 0;
 const TNA = 45/100;
+const porcentaje = 100;
+const none = "";
 
-const none = ""
-if (opcionesA==none){
-    alert("El campo no puede estar vacío.")
+
+if (opciones==none){
+    alert("El campo no puede estar vacío.");
 }
 
-//Funcionalidad de 1/2
-while (opciones != 0) {
-switch (opciones) {
+
+while (opciones!=3){
+    switch (opciones){
         case "1":
-            importe=prompt ("Ingrese importe a consultar")
-            if (importe > 0){
-                alert("Su importe a pagar es " + importe + TNA);
-            }
+            importe = prompt ("Ingrese el importe");
+            if (importe>0)
+                alert ("Su importe a pagar es " + importe);
             break;
-                default: //Añadir default.
-    }
-    opciones = prompt ("Bienvenido.\nSeleccione la Opción deseada.\n1- Calcular Préstamo(1 Pago)\n2- Calcular Prestamos 6 Cuotas Fijas  ");
+        
+        case "2":
+            cuotas = prompt ("Seleccione el numero de cuotas\n3 Cuotas\n6 Cuotas\n12 Cuotas");
+            importe = prompt ("Ahora ingrese su importe");
+            if (cuotas !=0)
+                cuotificar()=alert("Tus cuotas quedaran de la siguiente forma" + cuotas + " cuotas de " + cuotificar)
+
+            break;
+
+                default:
+                    "Vuelva al menú inicial";
+                    function cuotificar (importe,cuotas){
+                        resultado = importe/cuotas
+        }
+
+
+
+        opciones = prompt("Bienvenido.\nSeleccione la Opción deseada.\n1- Calcular Préstamo(1 Pago)\n2- Calcular Prestamos 6 Cuotas Fijas\n3-Salir.");
+}
+
+
 }
